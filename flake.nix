6edbb9a17481eb75b8b09046153f015eb33c364c
ahargunyllib <billy.bpm03@gmail.com>
2026-03-06 20:18:55 +0700

    chore: Add initial .gitignore, flake.lock, and flake.nix files for Nix configuration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "micromart's nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+  in
+  {
+    devShells."aarch64-darwin".default = pkgs.mkShell {
+      packages = [
+        pkgs.go
+      ];
+
+      shellHook = ''
+        echo "Welcome to the devShell!"
+      '';
+    };
+  };
+}
